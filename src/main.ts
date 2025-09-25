d6edc8da8628240f15daf4ef536c59cdc958e7e6
Newtonouma <armstrongnewton02@gmail.com>
2025-09-25 12:51:51 +0300

    change for cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,11 @@ async function bootstrap() {
   
   // Enable CORS for frontend
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'https://gravixel-attires-frontend.onrender.com'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
